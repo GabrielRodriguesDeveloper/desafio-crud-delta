07fff557a655cdd8c90eb3a9aa810d95c751448c
GabrielRodriguesDeveloper <gabrielsousa455@gmail.com>
2022-01-18 16:47:40 -0300

    validations

--- crud/urls.py
+++ crud/urls.py
@@ -11,10 +11,11 @@ urlpatterns = [
     path('cria_produtos/', views.cria_produtos, name="cria_produtos"),
     path('cria_categorias/', views.cria_categorias, name="cria_categorias"),
     path('produtos/ver_produto/<int:produto_id>', views.ver_produto, name="ver_produto"),
-    path('categorias/ver_categoria<int:categoria_id>', views.ver_categoria, name="ver_categoria"),
+    path('categorias/ver_categoria/<int:categoria_id>', views.ver_categoria, name="ver_categoria"),
     path('editar_produto/<int:produto_id>', views.editar_produto, name="editar_produto"),
     path('editar_categoria/<int:categoria_id>', views.editar_categoria, name="editar_categoria"),
     path('produto_update/<int:produto_id>', views.produto_update, name="produtoUpdate"),
+    path('categoria_update/<int:categoria_id>', views.categoria_update, name="categoriaUpdate"),
     path('deletar_produto/<int:produto_id>', views.deletar_produto, name="deletar_produto"),
     path('deletar_categoria/<int:categoria_id>', views.deletar_categoria, name="deletar_categoria"),
 ]
--- crud/views.py
+++ crud/views.py
@@ -45,50 +45,93 @@ def cria_categorias(request):
 
 
 def ver_produto(request, produto_id):
-    produto = Produtos.objects.get(pk=produto_id)
+    try:
+        produto = Produtos.objects.get(pk=produto_id)
+    except:
+        messages.add_message(request, messages.ERROR, "O produto não existe, tente colocar um id existente")
+        return redirect('produtos')
 
     return render(request, 'crud/ver_produto.html', {'produto' : produto})
 
 
 def ver_categoria(request, categoria_id):
-    categoria = Categoria.objects.get(pk=categoria_id)
+    try:
+        categoria = Categoria.objects.get(pk=categoria_id)
+    except:
+        messages.add_message(request, messages.ERROR, "A categoria não existe, tente colocar um id existente")
+        return redirect('categorias')
 
     return render(request, 'crud/ver_categoria.html', {'categoria' : categoria})
 
 
 
 def editar_produto(request, produto_id):
-    produto = Produtos.objects.get(pk=produto_id)
-    form = ProdutosForm(instance=produto)
+    try:
+        produto = Produtos.objects.get(pk=produto_id)
+        form = ProdutosForm(instance=produto)
+    except:
+        messages.add_message(request, messages.ERROR, "O produto não existe, tente colocar um id existente")
+        return redirect("produtos")
     return render(request, 'crud/produtos_form.html', {'produto' : produto, 'form' : form})
 
 
 def editar_categoria(request, categoria_id):
-    categoria = Categoria.objects.get(pk=categoria_id)
-    form = CategoriaForm(instance=categoria)
+    try:
+        categoria = Categoria.objects.get(pk=categoria_id)
+        form = CategoriaForm(instance=categoria)
+    except:
+        messages.add_message(request, messages.ERROR, "A categoria não existe, tente colocar um id existente")
+        return redirect("categorias")
+
     return render(request, 'crud/categorias_form.html', {'categoria' : categoria, 'form' : form})
 
 
 def produto_update(request, produto_id):
-    produto = Produtos.objects.get(pk=produto_id)
-    form = ProdutosForm(request.POST or None, instance=produto)
-    if form.is_valid():
-        form.save()
-        return redirect('index')
+    try:
+        produto = Produtos.objects.get(pk=produto_id)
+        form = ProdutosForm(request.POST or None, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('produtos')
+    except:
+        messages.add_message(request, messages.ERROR, "Você está tentando atualizar um produto inexistente, tente colocar um id existente")
+        return redirect("produtos")
+
+    return redirect('index')
+
+
+def categoria_update(request, categoria_id):
+    try:
+        categoria = Categoria.objects.get(pk=categoria_id)
+        form = CategoriaForm(request.POST or None, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('categorias')
+    except:
+        messages.add_message(request, messages.ERROR, "Você está tentando atualizar uma categoria inexistente, tente colocar um id existente")
+        return redirect("categorias")
+
     return redirect('index')
 
 
 def deletar_produto(request, produto_id):
-    produto = Produtos.objects.get(pk=produto_id)
-    produto.delete()
+    try:
+        produto = Produtos.objects.get(pk=produto_id)
+        produto.delete()
+    except:
+        messages.add_message(request, messages.ERROR, "Você está tentando deletar um produto inexistente, tente colocar id existente")
+        return redirect('produtos')
+
     return redirect('produtos')
 
 
+
 def deletar_categoria(request, categoria_id):
-    categoria = Categoria.objects.get(pk=categoria_id)
     try:
+        categoria = Categoria.objects.get(pk=categoria_id)
         categoria.delete()
     except:
-        messages.add_message(request, messages.ERROR, "A categoria não pode ser deletada, pois está sendo utilizada em um produto")
+        messages.add_message(request, messages.ERROR, "A categoria não pode ser deletada, pois está sendo utilizada em um produto ou não existe")
         return redirect('categorias')
+        
     return redirect('categorias')
